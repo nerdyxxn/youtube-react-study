32df8196401139fea4afd6f5dfb3454776d209b1
윤성 <yunyul1209@gmail.com>
2022-06-20 15:15:17 +0900

    feat(*) : Add video upload function
    비디오 업로드 기능 추가

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -3,6 +3,7 @@ import { Typography, Button, Form, message, Input } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const { Title } = Typography;
 const { TextArea } = Input;
@@ -21,7 +22,10 @@ const CategoryOptions = [
   { value: 3, label: 'Pets & Animals' },
 ];
 
-function VideoUploadPage() {
+function VideoUploadPage(props) {
+  //state에 있는 유저의 모든 정보 가져오기
+  const user = useSelector((state) => state.user);
+
   const [VideoTitle, setVideoTitle] = useState('');
   const [Description, setDescription] = useState('');
   const [Private, setPrivate] = useState(0);
@@ -95,12 +99,40 @@ function VideoUploadPage() {
     });
   };
 
+  // onSubmit func 생성
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const variables = {
+      writer: user.userData._id,
+      title: VideoTitle,
+      description: Description,
+      privacy: Private,
+      filePath: FilePath,
+      category: Category,
+      duration: Duration,
+      thumbnail: Thumbnail,
+    };
+
+    axios.post('/api/video/uploadVideo', variables).then((response) => {
+      if (response.data.success) {
+        // 비디오 업로드 성공 시
+        message.success('비디오 업로드 성공!');
+        setTimeout(() => {
+          props.history.push('/');
+        }, 3000);
+      } else {
+        alert('비디오 업로드에 실패했습니다.');
+      }
+    });
+  };
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
         <Title level={2}>Upload Video</Title>
       </div>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* Drop zone */}
           <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
@@ -157,7 +189,7 @@ function VideoUploadPage() {
         </select>
         <br />
         <br />
-        <Button type="primary" size="large">
+        <Button type="primary" size="large" onClick={onSubmit}>
           Submit
         </Button>
       </Form>
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Video } = require('../models/User');
+const { Video } = require('../models/Video');
 const { auth } = require('../middleware/auth');
 const multer = require('multer');
 const path = require('path');
@@ -48,6 +48,17 @@ router.post('/uploadfiles', (req, res) => {
         fileName: res.req.file.filename,
       });
     }
+  });
+});
+
+router.post('/uploadVideo', (req, res) => {
+  // 비디오 정보들을 mongoDB에 저장한다.
+  const video = new Video(req.body);
+  console.log(video);
+
+  video.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({ success: true });
   });
 });
 
